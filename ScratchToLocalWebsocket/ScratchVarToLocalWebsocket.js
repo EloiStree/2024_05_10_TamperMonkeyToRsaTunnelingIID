9b10ac63b8e6ea127618171bf624f5146e691bd2
Éloi Strée <eloistree@gmail.com>
2024-05-10 22:22:47 +0200

    save

--- ScratchToLocalWebsocket/ScratchVarToLocalWebsocket.js
+++ ScratchToLocalWebsocket/ScratchVarToLocalWebsocket.js
@@ -14,13 +14,31 @@
 
 import asyncio
 import websockets
-# create handler for each connection
+import socket
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+target_ip = "127.0.0.1"
+target_port = 7073
+
 async def handler(websocket, path):
-    data = await websocket.recv()
-    print(f"Data recieved as: {data}")
+    while True:
+        global target_port
+        data = await websocket.recv()
+        print(f"Key Value| {data}")
+        date_parts = data.split(":")
+        if(len(date_parts) == 2):
+            key = date_parts[0]
+            value = date_parts[1]
+            print(f"Key {key}  |  Value {value}")
+            try:
+                value = int(value)
+                value_bytes = value.to_bytes(2, byteorder='little')
+                udp_socket.sendto(value_bytes, (target_ip, target_port))
+                print(f"Push Integer| {value} |{value_bytes}")
+            except ValueError:
+                pass
 
 start_server = websockets.serve(handler, "localhost", 7072)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
@@ -31,46 +49,47 @@ asyncio.get_event_loop().run_forever()
 
     var socket = null; // Initialize WebSocket variable
     var previousData = {}; // Store the previous data
-    var useFullText = false;
     var useKeyValue = true;
 
     // Function to create WebSocket connection
     function connectWebSocket() {
         // Create WebSocket connection
+        if (socket === null || socket.readyState === WebSocket.CLOSED){
 
-        //  https://github.com/EloiStree/2024_02_01_UDPWebsocketRedirection
-        socket = new WebSocket('ws://localhost:7072');
+            //  https://github.com/EloiStree/2024_02_01_UDPWebsocketRedirection
+            socket = new WebSocket('ws://localhost:7072');
 
-        // WebSocket events
-        socket.addEventListener('open', function (event) {
-            console.log('WebSocket connected');
-        });
+            // WebSocket events
+            socket.addEventListener('open', function (event) {
+                console.log('WebSocket connected');
+            });
 
-        socket.addEventListener('message', function (event) {
-            console.log('Message from server:', event.data);
-        });
+            socket.addEventListener('message', function (event) {
+                console.log('Message from server:', event.data);
+            });
 
-        socket.addEventListener('close', function (event) {
-            console.log('WebSocket connection closed');
-            // Reconnect if connection closed unexpectedly
-            connectWebSocket();
-        });
+            socket.addEventListener('close', function (event) {
+                console.log('WebSocket connection closed');
+                // Reconnect if connection closed unexpectedly
+                connectWebSocket();
+            });
 
-        socket.addEventListener('error', function (event) {
-            console.error('WebSocket connection error:', event);
-        });
+            socket.addEventListener('error', function (event) {
+                console.error('WebSocket connection error:', event);
+            });
+        }
     }
 
     // Check WebSocket status and reconnect if necessary
     function checkWebSocketStatus() {
-        if (!socket || socket.readyState === WebSocket.CLOSED) {
+        if (socket===null || socket.readyState === WebSocket.CLOSED) {
             console.log('WebSocket is not open. Reconnecting...');
             connectWebSocket();
         }
     }
     function sendText(data) {
         if (socket && socket.readyState === WebSocket.OPEN) {
-            socket.send(data);
+           // socket.send(data);
         }
     }
     // Send data to WebSocket server
@@ -115,10 +134,7 @@ asyncio.get_event_loop().run_forever()
                 }
             }
         }
-        if (useFullText && previousData.fullText !== dataString) {
-            previousData.fullText = dataString;
-            sendText("<FullData>\n"+ dataString + "\n</FullData>");
-        }
+       
         //console.timeEnd('extractAndSendData'); // End the timer and display the elapsed time
     }
 
