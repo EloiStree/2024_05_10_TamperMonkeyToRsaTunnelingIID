5edaf13cd78dec7f61409406fecdfd71057451dc
Eloi Strée <eloistree@gmail.com>
2024-08-29 23:31:44 +0200

    save

--- ScratchToLocalWebsocket/LocalWebsocketServer.py
+++ ScratchToLocalWebsocket/LocalWebsocketServer.py
@@ -3,15 +3,21 @@ import websockets
 import socket
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-target_ip = "127.0.0.1"
-target_port = 7073
+server_websocket_port= 7072
+target_udp_ip = "127.0.0.1"
+target_udp_port = 7073
+
+bool_display_received = True
 
 async def handler(websocket, path):
     while True:
         global target_port
         data = await websocket.recv()
-        print(f"Key Value| {data}")
-        date_parts = data.split(":")
+        data_as_string = str(data)
+        
+        if bool_display_received:
+            print(f"Received| {data_as_string}")
+        date_parts = data_as_string.split(":")
         if(len(date_parts) == 2):
             key = date_parts[0]
             value = date_parts[1]
@@ -19,11 +25,11 @@ async def handler(websocket, path):
             try:
                 value = int(value)
                 value_bytes = value.to_bytes(2, byteorder='little')
-                udp_socket.sendto(value_bytes, (target_ip, target_port))
+                udp_socket.sendto(value_bytes, (target_udp_ip, target_udp_port))
                 print(f"Push Integer| {value} |{value_bytes}")
             except ValueError:
                 pass
             
-start_server = websockets.serve(handler, "localhost", 7072)
+start_server = websockets.serve(handler, "localhost", server_websocket_port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
